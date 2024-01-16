3a68293e72367ab617b210050da2741425c8f28b
Alex Demin <alex66690@inbox.ru>
2024-01-16 16:39:44 +0300

    all ok

---
+++ myproject/new python.py
@@ -0,0 +1,8 @@
+for a in range(64):
+    f=True
+    for x in range(64):
+        if (((x&42!=0)or(x&13!=0))<=((x&30==0)<=(x&a!=0)))==False:
+            f=False
+    if f:
+        print(a)
+        break
